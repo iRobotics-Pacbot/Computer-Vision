c3258bd3b85e559ff3bf3779c08d238043dd88a4
Joshua Chen <jhc3628@gmail.com>
2024-11-03 13:35:46 -0600

    Publish Branch

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <filesystem>
+
+int main() {
+    std::filesystem::path path = "testImages";
+    cv::waitKey(0);
+}
