91ef16fa83e974257f882b6baa8dd130b7b32989
joshuachen6 <jhc3628@gmail.com>
2025-03-29 14:53:18 -0500

    better info

--- src/Config.cpp
+++ src/Config.cpp
@@ -11,29 +11,29 @@ void Config::loadFromFile(const std::string &path) {
     YAML::Node data = YAML::LoadFile(path);
 
     if (data["process"]) {
-      spdlog::debug("Process {}", process = data["process"].as<std::string>());
+      spdlog::debug("Process: {}", process = data["process"].as<std::string>());
     }
 
     if (data["pipeline"]) {
-      spdlog::debug("Pipeline {}",
+      spdlog::debug("Pipeline: {}",
                     pipeline = data["pipeline"].as<std::string>());
     }
 
     if (data["calibrator"]) {
-      spdlog::debug("Calibrator {}",
+      spdlog::debug("Calibrator: {}",
                     calibrator = data["calibrator"].as<std::string>());
     }
 
     if (data["cameraIndex"]) {
-      spdlog::debug("CameraIndex {}",
+      spdlog::debug("CameraIndex: {}",
                     cameraIndex = data["cameraIndex"].as<int>());
     }
 
     if (data["exposure"]) {
-      spdlog::debug("Exposure {}", exposure = data["exposure"].as<int>());
+      spdlog::debug("Exposure: {}", exposure = data["exposure"].as<int>());
     }
   } catch (YAML::ParserException &e) {
-    spdlog::critical("Failed to read yaml: {}", path);
+    spdlog::critical("Failed to read configuration yaml: {}", path);
     throw e;
   }
 }
